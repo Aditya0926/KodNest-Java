2313ff07f536ed789bb416d8d6b14958d6f526d1
Aditya0926 <aditya602121.com>
2026-09-07 14:36:01 +0530

    goal

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -3,5 +3,6 @@ public class WelcomeToJava {
        //Write your code here
        System.out.println("Welcome to Java"); 
        System.out.println("I am in java Track");
+       System
     }
 }
---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/commitment.java
@@ -0,0 +1,8 @@
+class commitment{
+    public static void main(String[] args) {
+        //Display the students learning track
+        System.out.println("Track: Java development");
+        //Display the students daily commitment
+        System.out.println("Daily practice: 8 Hours");
+    }
+}
